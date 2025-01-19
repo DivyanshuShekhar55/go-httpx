e05ebc7199bbb07b8812fae093de140b38540d6d
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-19 09:37:51 +0530

    updated gitignore and server file

--- app/server.go
+++ app/server.go
@@ -8,12 +8,15 @@ import (
 
 func main() {
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	// create a tcp-listener
+	lis, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	// accept connections by keeping the server forever on...
+	_, err = lis.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
