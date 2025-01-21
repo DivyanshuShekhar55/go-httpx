33c58ca48c3e79529a402c980f4bbdf1e1a4e179
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-21 19:55:55 +0530

    updated the server.go code as per new files

--- app/server.go
+++ app/server.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"net"
 	"os"
 
-	"github.com/DivyanshuShekhar55/go-htttpx/app/internals/path"
 )
 
 func main() {
@@ -27,27 +25,6 @@ func main() {
 	// close the connection when not needed
 	defer conn.Close()
 
-	// Next, lets get the url requested, a GET looks like this :GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n
-	// send 404 for any other path than the home route
-	buffer := make([]byte, 10240)
-	_, err = conn.Read(buffer)
-	if err != nil {
-		fmt.Println("error while reading the request buffer")
-	}
-
-	
-	// buffer is a long sequence of bytes (like 12 17 ... 0 0 ...0)
-	// convert it to readable string, using the bytes package
-	buf := bytes.NewBuffer(buffer)
-	fullString := buf.String() //returns full req string
-	fmt.Println(fullString)
-
-	route := path.GetPath(buf.String())
-	msg := Router(route, fullString)
-
-	_, err = conn.Write([]byte(msg))
-	if err != nil {
-		fmt.Println("error writing over connection")
-	}
+	HandleReq(conn)
 
 }
