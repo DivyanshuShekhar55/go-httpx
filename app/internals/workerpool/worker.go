eeec9c34994caf6cfdc2d12a017a7d7b6c576eaa
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-22 09:12:03 +0530

    added the code for workers

--- app/internals/workerpool/worker.go
+++ app/internals/workerpool/worker.go
@@ -1,17 +1,47 @@
 package workerpool
 
-import "net"
+import (
+	"net"
 
+	"github.com/DivyanshuShekhar55/go-htttpx/app/internals/handler"
+)
+
+// the done channel is used to stop/quit the worker pool
+// unbuffered channel that signals a message if we need to stop
+// see concurrency pattern videos from Kantan Coding
 type Worker struct {
-	ID int
+	ID       int
 	jobQueue chan net.Conn
+	done     chan bool
 }
 
-func NewWorker(id int, jobQueue chan net.Conn) *Worker{
+func NewWorker(id int, jobQueue chan net.Conn) *Worker {
 	return &Worker{
-		ID : id,
+		ID:       id,
 		jobQueue: jobQueue,
+		done:     make(chan bool),
 	}
 }
 
+func (w *Worker) Start() {
+	go func() {
+		for {
+			select {
 
+			// if a connection is pushed into the queue/channel, take it up
+			// pop out one element from the job channel, which is just a request
+			case conn := <-w.jobQueue:
+				// process the req
+				handler.HandleReq(conn)
+
+			case <-w.done:
+				return
+			}
+		}
+	}()
+}
+
+func (w *Worker) Stop() {
+	// as soon as the stop message passed into queue / channel, the unbuffered channel pushes it to the consumer inside the Start func
+	w.done <- true
+}
