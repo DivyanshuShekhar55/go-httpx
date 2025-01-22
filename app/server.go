abc5da7b439b2ba6c781e8a1835b10583e8e07cf
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-22 19:35:40 +0530

    Stage -6 : COMPLETED. added the main server code to create new pool and run concurrent requests

--- app/server.go
+++ app/server.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"os"
 
+	"github.com/DivyanshuShekhar55/go-htttpx/app/internals/workerpool"
 )
 
 func main() {
@@ -16,15 +17,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	// accept connections by keeping the server on...will shut once a connection is over
-	conn, err := lis.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	// create a worker-pool with max 2 workers
+	pool := workerpool.NewPool(2)
+	defer pool.Stop()
+
+	// main server loop ... accept connections by keeping the server on
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		pool.Submit(conn)
 	}
-	// close the connection when not needed
-	defer conn.Close()
-
-	HandleReq(conn)
-
 }
