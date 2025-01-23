23176d418da1f9140b8e27d48e3629fd004edd55
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-23 13:09:42 +0530

    added code for getting the req-method from req

---
+++ app/internals/path/getMethod.go
@@ -0,0 +1,14 @@
+package path
+
+import "strings"
+
+func Method(reqStr string)string{
+	
+	// separate the parameters by space
+	// reqStr is like: POST /files/number HTTP/1.1\r\nHost: ...
+	separated_params := strings.Split(reqStr, " ")
+	
+	// get the first item ...
+	method := separated_params[0]
+	return method
+}
