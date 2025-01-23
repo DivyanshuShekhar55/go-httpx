9cc4a45adbf954aa17d52b63d6792f35a09b2c46
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-23 13:09:10 +0530

    added code for getting the body from the req

--- app/internals/path/getBody.go
+++ app/internals/path/getBody.go
@@ -1,1 +1,17 @@
 package path
+
+import (
+	"strings"
+)
+
+func Body(reqStr string) string{
+
+	// separate the parameters by \r\n 
+	separated_params := strings.Split(reqStr, "\r\n")
+	
+	// grab the \r\n at the last of the params array 
+	body := separated_params[len(separated_params)-1]
+
+	return body
+
+}
