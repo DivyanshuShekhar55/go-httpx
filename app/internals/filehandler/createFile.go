de47de15d7ad4706606fa13a379a8ddd7b250194
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-23 13:08:53 +0530

    added code for creating file

--- app/internals/filehandler/createFile.go
+++ app/internals/filehandler/createFile.go
@@ -1,1 +1,28 @@
 package filehandler
+
+import (
+	"fmt"
+	"os"
+)
+
+func CreateFile(fileName, text string) error {
+
+	// create a new file ...
+	file, err := os.Create(fileName)
+	if err!=nil{
+		fmt.Println(err)
+		return err
+	}
+
+	// write on the file ...
+	// val returned on success is length but we don't need it so using an '_'
+	_, err= file.WriteString(text)
+
+	if err!=nil{
+		fmt.Println(err)
+		return err
+	}
+
+	return nil
+
+}
