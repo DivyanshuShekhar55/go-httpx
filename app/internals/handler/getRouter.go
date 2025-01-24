30aae70788b5ddb63b00b7fe01bba028413190fc
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-01-24 19:53:38 +0530

    changed the implementation of sending the response message

--- app/internals/handler/getRouter.go
+++ app/internals/handler/getRouter.go
@@ -7,18 +7,27 @@ import (
 
 	"github.com/DivyanshuShekhar55/go-htttpx/app/internals/filehandler"
 	"github.com/DivyanshuShekhar55/go-htttpx/app/internals/req"
+	"github.com/DivyanshuShekhar55/go-htttpx/app/types"
 )
 
 func Get(route string, fullString string) (msg string) {
 
 	switch {
 	case route == "/":
-		msg = "HTTP/1.1 200 OK\r\n\r\n"
+		msg = types.NewResponse(200, types.NewTextHeader(), "ok")
 
 	case strings.HasPrefix(route, "/echo"):
+
 		content := req.NestedPath(route, 1)
 		content_len := strconv.Itoa(len(content))
-		msg = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %s\r\n\r\n%s", content_len, content)
+		header := types.Header{
+			Fields: map[string]string{
+				"Content-Type":   "text/plain",
+				"Content-Length": string(content_len),
+			},
+		}
+
+		msg = types.NewResponse(200, header, content)
 
 	case route == "/user-agent":
 
